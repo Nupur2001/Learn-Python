943459b1762499d33c0cacdc0f494f19d7a414f0
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-05-10 23:43:42 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+# Single Line Comment -> Use #
+# print("Hello World")
+
+# Multi Line Comment -> Use '''
+'''
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+'''
