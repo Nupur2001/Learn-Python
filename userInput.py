ddb2ae043d2041da75338a292d7957cdf8792699
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-05-11 13:31:20 +0530

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,14 @@
+name=input("Enter your name: ")
+print(name)
+print(type(name))
+
+num=input("Enter your birth date: ")
+print(type(num))
+
+num=int(input("Enter your birth date: "))
+print(type(num))
+print(num)
+
+num=int(input("Enter a number: "))
+print(num+10)
+print(type(num))
