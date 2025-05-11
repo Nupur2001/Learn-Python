6b57052906e946de08977d57da7c8cf56695d150
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-05-11 09:07:49 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+a=3 #Assignment Operators
+b=2 #Assignment Operators
+
+#Arithmetic Operators
+add=a+b
+minus=a-b
+multiply=a*b
+divide=a/b
+floorDivide=a//b
+modulus=a%b
+power=a**b
+
+print("Add a+b= ",add)
+print("Minus a-b= ",minus)
+print("Multiply a*b= ",multiply)
+print("Divide a/b= ",divide)
+print("Floor Divide a//b= ",floorDivide)
+print("Modulus a%b= ",modulus)
+print("Power a**b= ",power)
+
+
+
+#Comparison Operators
+print("a>b",a>b)
+print("a<b",a<b)
+print("a==b",a==b)
