c48117682a35fe16f5dfa8a1d578e4e80542da56
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-06-19 21:34:49 +0530

    Create Pattern2.py

---
+++ Patterns/Number Pattern/Pattern2.py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+# Output
+Enter the number of rows: 4
+1
+22
+333
+4444
