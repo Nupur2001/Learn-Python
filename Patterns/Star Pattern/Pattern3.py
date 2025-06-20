a97a875155680328a47f7fe290cbbacc1dfa79bc
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-06-20 22:12:46 +0530

    Update Pattern3.py

--- Patterns/Star Pattern/Pattern3.py
+++ Patterns/Star Pattern/Pattern3.py
@@ -4,14 +4,6 @@ for i in range(0,n):
         print("*",end="")
     print()
 
-n=int(input("Enter the number of rows: "))
-for i in range(1, n+1):
-    for j in range(0,n-i-1):
-        print(" ")
-    for k in range(0, (2*i)+1):
-        print("*")
-    print()
-
 # Output
 Enter the number of rows: 4
 ****
