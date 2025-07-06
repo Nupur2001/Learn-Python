44a1fef6d7ea6d8f42facee576790e92efee2ac4
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-07-06 22:28:20 +0530

    Update PrintAllDivisors.py

--- Basic Maths/PrintAllDivisors.py
+++ Basic Maths/PrintAllDivisors.py
@@ -1,10 +1,32 @@
-num=int(input("Enter a number: "))
+n=int(input("Enter a number: "))
 divisors=[]
-for i in range(1,num+1):
-    if num% i==0:
+for i in range(1,n+1):
+    if n% i==0:
         divisors.append(str(i))
-print("The divisors of", num, "are:", ", ".join(divisors))
+print("The divisors of", n, "are:", ", ".join(divisors))
 
 # Output
 Enter a number: 36
 The divisors of 36 are: 1, 2, 3, 4, 6, 9, 12, 18, 36
+
+from math import isqrt
+
+
+num=int(input("Enter a number: "))
+divisors=[]
+for i in range(1,isqrt(num)+1):
+    if num % i == 0:
+        divisors.append(str(i))
+        if num/i !=i:  
+            divisors.append(str(num // i))
+divisors.sort()
+divisorsStr=[str(d) for d in divisors]
+print("The divisors of", num, "are:", ", ".join(divisorsStr))
+
+# Output
+Enter a number: 26
+The divisors of 26 are: 1, 13, 2, 26
+Enter a number: 36
+The divisors of 36 are: 1, 12, 18, 2, 3, 36, 4, 6, 9
+
+
